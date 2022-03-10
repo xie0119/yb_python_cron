1648ae530c4247ea2399db6eee8e064b1a808131
Xie <42569108+xie0119@users.noreply.github.com>
2022-03-10 20:59:50 +0800

    每个账号尝试登录3次

--- yb_cookie.py
+++ yb_cookie.py
@@ -30,24 +30,32 @@ if __name__ == '__main__':
             lit = i['remarks'].split('|')
             account = lit[0]
             password = lit[1]
-            result = yb.chrome_login(account, password)
+
+            yiban_token = i['value']
+            status = 0
+            for ct in range(3):
+
+                result = yb.chrome_login(account, password)
+
+                if result['code'] == 711:
+                    print('Cookie: 第 %d次登录失败 %s %s' % (ct, account, result['message']))
+                    continue
+
+                if result['code'] != 200:
+                    print('Cookie: 登录失败 %s %s' % (account, result['message']))
+                    status = 2
+                    continue
+                yiban_token = f'yiban_user_token={result["yiban_user_token"]};'
+                result = api.update_envs(i['id'], i['name'], yiban_token, i['remarks'])
+                break
 
             temp = {
                 'name': i['name'],
+                'value': yiban_token,
                 'remarks': i['remarks'],
                 'id': i['id'],
+                'status': status
             }
-
-            if result['code'] != 200:
-                temp['value'] = i['value']
-                temp['status'] = 2
-                user_list.append(temp)
-                print('Cookie: 登录失败 %s %s' % (account, result['message']))
-                continue
-            yiban_token = f'yiban_user_token={result["yiban_user_token"]};'
-            result = api.update_envs(i['id'], i['name'], yiban_token, i['remarks'])
-            temp['value'] = yiban_token
-            temp['status'] = 0
             user_list.append(temp)
             print('Cookie: 更新成功 user: %s token: %s' % (account, yiban_token))
         except Exception as ex:
